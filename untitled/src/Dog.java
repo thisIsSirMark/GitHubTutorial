261ae6ed6f501f0df8ba014152e7e74f11b1e328
markyyy <markjaysonlanuzo9@gmail.com>
2024-02-29 11:38:18 +0800

    Added Dog class 2/29/24

---
+++ untitled/src/Dog.java
@@ -0,0 +1,19 @@
+public class Dog {
+
+    String name;
+    int age;
+
+    public Dog(){
+
+    }
+
+    public Dog(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public void eat(){
+        System.out.println("NUM! NUM! NUM!");
+    }
+
+}
