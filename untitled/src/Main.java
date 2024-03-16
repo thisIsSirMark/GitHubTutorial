df1dda7caa6175e0ece924f7ee3fbd943df2f9f9
thisIsSirMark <145762444+thisIsSirMark@users.noreply.github.com>
2024-03-16 08:19:54 +0000

    h

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,8 +1,17 @@
 import animal.Animal;
 import animal.Dog;
+import java.util.Scanner;
+
+import javax.swing.JFrame;
 
 public class Main {
+
+    
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        JFrame mFrame = new JFrame();
+
+    
         Dog myDog = new Dog();
 
         myDog.eat();
@@ -10,6 +19,11 @@ public class Main {
 
         myDog.name = "kobi";
 
+        System.out.print("Enter dogs name: ");
+		myDog.name = sc.nextLine();
 
+        System.out.println("Your dog's name is "+myDog.name +".");
+
+        sc.close();
     }
 }
