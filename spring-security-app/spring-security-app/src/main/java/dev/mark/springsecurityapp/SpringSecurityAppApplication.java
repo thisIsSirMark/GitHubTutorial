3f37025530ef25d02b8010eb04c12cf850a5c8f7
markyyy <markjaysonlanuzo9@gmail.com>
2024-04-06 10:37:13 +0800

    init

---
+++ spring-security-app/spring-security-app/src/main/java/dev/mark/springsecurityapp/SpringSecurityAppApplication.java
@@ -0,0 +1,14 @@
+package dev.mark.springsecurityapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class SpringSecurityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAppApplication.class, args);
+	}
+
+}
---
+++ spring-security-app/spring-security-app/src/test/java/dev/mark/springsecurityapp/SpringSecurityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mark.springsecurityapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
